3730714bb7e002e39d70551630c3053f9fbf851d
takishun <takishun.st3260@gmail.com>
2021-06-29 13:00:29 +0900

    frame setup

--- Frame/Frame.py
+++ Frame/Frame.py
@@ -1,13 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
 from Window import WindowTitle
 
+from PIL import ImageGrab
+from win32gui import GetWindowText, GetForegroundWindow
 
 class Frame:
 
     def show_selection(self):
         for i in self.listbox.curselection():
-            print(self.listbox.get(i))
+            self.current_window['text'] = str(self.listbox.get(i)) + '　　を選択中'
+
+    def take_screenshot(self):
+        ImageGrab.grab().save('Image.png')
+        
 
     def __init__(self, main_win):
         
@@ -17,12 +24,29 @@ class Frame:
                 
         windows = WindowTitle.get_window_title()
         var = tk.StringVar(value=windows)
-        self.listbox = tk.Listbox(self.frame, listvariable=var, height=5, width=20)
+        self.listbox = tk.Listbox(self.frame, listvariable=var, height=10, width=80)
 
         self.listbox.bind('<<ListboxSelect>>', lambda e: self.show_selection())
         self.listbox.grid(row=0,column=0)
 
-        button = ttk.Button(self.frame, text='OK', command=lambda: self.show_selection(),)
-        button.grid(row=0, column=1)
-        self.frame.pack(padx=10, pady=20)
+            # Scrollbar
+        scrollbar = ttk.Scrollbar(
+            self.frame,
+            orient=VERTICAL,
+            command=self.listbox.yview)
+        self.listbox['yscrollcommand'] = scrollbar.set
+        scrollbar.grid(row=0, column=1, sticky=(N, S))
+
+        self.title_select_button = ttk.Button(self.frame, text='設定', command=lambda: self.show_selection())
+        self.title_select_button.grid(row=2, column=0)
+
+        self.current_window = ttk.Label(text='未設定')
+        self.current_window.grid(row=5, column = 0)
+
+        
+        self.screenshot_button = ttk.Button(self.frame, text='スクリーンショット開始', command=self.take_screenshot)
+        self.screenshot_button.grid(row=6,column=1)
+        
+
+        self.frame.grid(padx=30, pady=30)
     
